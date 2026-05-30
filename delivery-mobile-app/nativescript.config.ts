ea321a45c798873849c57bf13c4229b08f8e9c71
Carranza506 <andres.carranza.moyano@est.una.ac.cr>
2026-05-30 14:44:20 -0600

    new base

--- delivery-mobile-app/nativescript.config.ts
+++ delivery-mobile-app/nativescript.config.ts
@@ -2,7 +2,7 @@ import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
   id: 'org.nativescript.deliverymobileapp',
-  appPath: 'src',
+  appPath: 'app',
   appResourcesPath: 'App_Resources',
   android: {
     v8Flags: '--expose_gc',
